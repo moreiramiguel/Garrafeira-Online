91361df22c30efdfb74b735533a65cdea8496e3e
M10D12 <107938456+M10D12@users.noreply.github.com>
2023-05-28 02:53:41 +0100

    fornecedores

--- Garrafeira/Fornecedores.cs
+++ Garrafeira/Fornecedores.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Garrafeira
 {
@@ -65,6 +67,132 @@ namespace Garrafeira
         private void tabPage2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string FornecedorName = textBox2.Text;
+            string FornecedorPhone = textBox4.Text;
+            string FornecedorNIF = textBox3.Text;
+            byte[] imageBytes = null;
+
+
+            OpenFileDialog dialog = new OpenFileDialog();
+            //dialog.Filter = "jpg files";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string imagePath = dialog.FileName;
+                imageBytes = File.ReadAllBytes(imagePath);
+            }
+
+
+            if (verifyFornecedor(FornecedorNIF))
+            {
+                if (insertFornecedores(FornecedorNIF, FornecedorPhone, FornecedorName, imageBytes))
+                {
+                    MessageBox.Show("Fornecedor adicionado");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("O Forncedor já se encontra na Base de Dados");
+            }
+
+        }
+
+        public bool verifyFornecedor(String FornecedorNIF)
+        {
+            string connectionString = "Data Source=LAPTOP-ICOK0BQ9;Initial Catalog=Garrafeira;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "SELECT NIF FROM Fornecedores WHERE NIF = @FornecedorNIF";
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@FornecedorNIF", FornecedorNIF);
+
+                object result = command.ExecuteScalar();
+
+                if (result != null)
+                {
+
+                    return false;
+                }
+                else
+                {
+
+                    return true;
+
+                }
+            }
+        }
+        public bool insertFornecedores(String FornecedorNIF, String FornecedorName, String FornecedorPhone, byte[] imageBytes)
+        {
+            string connectionString = "Data Source=LAPTOP-ICOK0BQ9;Initial Catalog=Garrafeira;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "INSERT INTO Fornecedores (Nome,Contacto, NIF,Image) VALUES (@FornecedorName, @FornecedorPhone, @FornecedorNIF,@Imagem)";
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@FornecedorName", FornecedorName);
+                command.Parameters.AddWithValue("@Imagem", imageBytes);
+                command.Parameters.AddWithValue("@FornecedorPhone", FornecedorPhone);
+                command.Parameters.AddWithValue("@FornecedorNIF", FornecedorNIF);
+
+
+                int rowsAffected = command.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=LAPTOP-ICOK0BQ9;Initial Catalog=Garrafeira;Integrated Security=True";
+            string sql = "SELECT * FROM Fornecedores";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
+                System.Data.DataTable dataTable = new System.Data.DataTable();
+                adapter.Fill(dataTable);
+
+                DataGrid dataGrid = new DataGrid();
+                dataGrid.DataSource = dataTable;
+
+                tabPage1.Controls.Add(dataGrid);
+                dataGrid.Width = 500;
+                dataGrid.Height = 300;
+
+            }
         }
     }
 }
