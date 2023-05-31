1ced566320e684827e93a6486075be0c1b8dc55f
Miguel Moreira <83714496+moreiramiguel@users.noreply.github.com>
2023-05-31 19:18:33 +0100

    update

--- Garrafeira/Empregados.cs
+++ Garrafeira/Empregados.cs
@@ -17,8 +17,6 @@ namespace Garrafeira
     public partial class Empregados : Form
     { 
 
-
-
         public Empregados()
         {
             InitializeComponent();
--- Garrafeira/Exemplo.Designer.cs
+++ Garrafeira/Exemplo.Designer.cs
@@ -29,12 +29,64 @@ namespace Garrafeira
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panelSideBar = new System.Windows.Forms.Panel();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelSideBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelSideBar
+            // 
+            this.panelSideBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(83)))), ((int)(((byte)(102)))));
+            this.panelSideBar.Controls.Add(this.button1);
+            this.panelSideBar.Controls.Add(this.panelLogo);
+            this.panelSideBar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSideBar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panelSideBar.Location = new System.Drawing.Point(0, 0);
+            this.panelSideBar.Name = "panelSideBar";
+            this.panelSideBar.Size = new System.Drawing.Size(200, 529);
+            this.panelSideBar.TabIndex = 0;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(41)))), ((int)(((byte)(47)))));
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(200, 85);
+            this.panelLogo.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(0, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Exemplo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(88)))), ((int)(((byte)(100)))), ((int)(((byte)(137)))));
+            this.ClientSize = new System.Drawing.Size(1147, 529);
+            this.Controls.Add(this.panelSideBar);
+            this.Name = "Exemplo";
             this.Text = "Exemplo";
+            this.panelSideBar.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelSideBar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelLogo;
     }
 }
